dbf76452bec966e2fdc66f2a7357bd3be1ddb69f
Cristian Ferrero <cristian.ferrero1992@gmail.com>
2016-04-14 15:26:09 -0300

    Agregado de la biblioteca

---
+++ Bibliotecas/Utiles/Utiles.c
@@ -0,0 +1,140 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+/** \brief Pregunta al usuario si desea continuar.
+ *
+ * \return int Si el usuario eligió NO continuar 0(cero), si el usuario eligió continuar 1(uno).
+ *
+ */
+int preguntarDeseaContinuar()
+{
+    char respuesta; //Respuesta que ingresa el usuario
+    int contador = 0;
+
+
+    do{
+        if(contador==0)
+        {
+            printf("Desea continuar? (s/n)");
+        }
+        else
+        {
+            printf("ERROR! Desea continuar? (s/n) ");
+        }
+        fflush(stdin);
+        scanf("%c", &respuesta);
+        respuesta=tolower(respuesta);
+        contador++;
+    }while( respuesta!='s' && respuesta!='n');
+
+    switch(respuesta)
+    {
+        case 's':
+            return 1;
+            break;
+        case 'n':
+            return 0;
+            break;
+    }
+    return 0;
+}
+
+
+/** \brief Valida si un número es entero, o con decimal.
+ *
+ * \param float Número a analizar.
+ *
+ * \return int Si el número es entero 1(uno), si el número tiene decimales 0(cero).
+ *
+ */
+int esEntero(float numero)
+{
+    float auxiliar;
+    auxiliar = numero - (int)numero;
+    if(auxiliar == 0)
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+/** \brief Analiza una cádena de caracteres y nos dice si dicha cádena es exactamente igual a "0".
+ *
+ * \param char[] Cádena a analizar.
+ *
+ * \return int Si no es una cadena de la forma "0" retorna 0(cero). Si la cadena es igual a "0" retorna 1(uno).
+ *
+ */
+int esStringCero(char* cadena)
+{
+    if(cadena[0]=='0' && cadena[1]=='\0')
+    {
+        return 1;
+    }
+    else
+    {
+        return 0;
+    }
+}
+
+
+/** \brief Muestra un mensaje, lee una entrada por teclado, valida que sea entero y que no sobrepase los límites dados.
+ *
+ * \param int* Donde se almacena el entero, solo en caso de ingreso exitoso.
+  *
+ * \param char* Mensaje mostrado al usuario cuando se pide el ingreso.
+  *
+ * \param int Límite inferior.
+  *
+ * \param int Límite superior.
+ *
+ * \return char* Mensaje que se muestra al usuario en caso de error.
+ *
+ */
+int getInt(int* retorno, char* mensaje, int minimo, int maximo, char* msjError)
+{
+    char buffer[601];
+    int aux;
+
+    printf("%s", mensaje); //Pido el ingreso del numero
+
+    fflush(stdin);
+
+    gets(buffer); //Leo el ingreso del usuario
+
+    if(!esStringCero(buffer))
+    {
+        aux = atoi(buffer);
+        if(aux == 0)
+        {
+            printf("%s", msjError);
+            return -1;
+        }
+        else
+        {
+            if(aux>minimo || aux<maximo)
+            {
+                *retorno = aux;
+                return 1;
+            }
+            else
+            {
+                printf("%s", msjError);
+                return -1;
+            }
+        }
+    }
+    else
+    {
+        *retorno = 0;
+        return 1;
+    }
+
+
+}
---
+++ Bibliotecas/Utiles/Utiles.h
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+
+//Prototipos de funciones
+
+int preguntarDeseaContinuar();
+
+int esEntero(float numero);
+int esStringCero(char* cadena)
+
+int getInt(int* retorno, char* mensaje, int minimo, int maximo, char* msjError);
