527e6f28707c98e80fe3f8c2a36d8e570c16a8b1
Cristian <cristian.ferrero1992@gmail.com>
2016-05-17 22:09:58 -0300

    clase 17

--- Clase 17 - Memoria dinamica/Ejercicio1/main.c
+++ Clase 17 - Memoria dinamica/Ejercicio1/main.c
@@ -1,8 +1,29 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+typedef struct{
+    int legajo;
+    char nombre[31];
+    float salario;
+}eEmpleado;
+
 int main()
 {
-    printf("Hello world!\n");
+    int *a;
+
+    a = (int*)malloc(sizeof(int));
+
+    if( a == NULL )
+    {
+        printf("No hay memoria suficiente.\n\n");
+        exit(0);
+    }
+    else
+    {
+        *a = 87;
+    }
+
+    free(a);
+
     return 0;
 }
